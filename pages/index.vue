8bc4c374817dcf053a4f219058e2d0ea0855d98d
nikosajadi <na.sajadii.29@gmail.com>
2024-02-13 10:55:11 +0100

    [crate]add list with Pinia method

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+
+<template>
+<div>
+    <Form />
+    <!-- <div>
+        <nuxt-link to="/a"> ADD</nuxt-link>
+        
+    </div> -->
+</div>
+    
+
+</template>
---
+++ store/piniaa.ts
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore("counter", {
+  state: () => ({
+    personlist: []
+  }),
+
+  getters: {
+  
+    getPersons: (state) => {
+      return (state.personlist)
+    },
+  },
+
+  actions: {
+    pushPerson(_name: string,_family: string) {
+      this.personlist.push(
+        {name: _name, family: _family }
+        )
+  },
+}
+});
