4603399215c19f3811fb720820cb8ea64cbf2516
nikosajadi <na.sajadii.29@gmail.com>
2024-02-21 13:23:25 +0100

    [feature]add cookie in login page

--- pages/practice1/login.vue
+++ pages/practice1/login.vue
@@ -1,23 +1,25 @@
 <script setup>
+
 import { toast } from 'vue3-toastify';
 import "vue3-toastify/dist/index.css";
 const username = ref("");
 const password = ref("");
-
+const router = useRouter();
 let goToLogin = () => {
     if (username.value =="" )  notify("Please input your email...");
  else if (password.value =="" ) notify("Please input your Password...");
   else {
-        if (username.value == "admin" & password.value == "admin") 
-        notify("it is  ok");
-    else {
-         notify("you are not admin");
-    }
-
-       }
-    }
-  
-   const notify = (content) => {
+        if (username.value == "admin" & password.value == "admin") {
+            const my_token = useCookie("token", { maxAge: 2000 });
+            my_token.value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik5hc2liZWggU2FkYXQiLCJmYW1pbHkiOiJTYWphZGkiLCJpYXQiOjE1MTYyMzkwMjJ9.AEOA_sbqPUYCrOvqKbTQ9fZLP3Mxxb2UkBvRvXhsb-U";
+           router.push({
+            path: "/practice1/",
+              });
+             }else {  notify("you are not admin");
+          } 
+             }
+  }
+    const notify = (content) => {
     toast(content, {
   "theme": "dark",
   "type": "faild",
@@ -27,58 +29,113 @@ let goToLogin = () => {
   "dangerouslyHTMLString": true
 })
 }
- 
 </script>
 <template>
+  <div>
     <div>
-        
-        <div>
-            <section class="bg-gray-50 dark:bg-gray-900">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-          Flowbite    
-      </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
+      <section class="bg-gray-50 dark:bg-gray-900">
+        <div
+          class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+        >
+          <a
+            href="#"
+            class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              class="w-8 h-8 mr-2"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              alt="logo"
+            />
+            Flowbite
+          </a>
+          <div
+            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+          >
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1
+                class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+              >
+                Sign in to your account
               </h1>
               <form class="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input  v-model="username" type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com">
-                      {{ username }}
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Your email</label
+                  >
+                  <input
+                    v-model="username"
+                    type="text"
+                    name="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                  />
+                  {{ username }}
+                </div>
+                <div>
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Password</label
+                  >
+                  <input
+                    v-model="password"
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div class="flex items-center justify-between">
+                  <div class="flex items-start">
+                    <div class="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      />
+                    </div>
+                    <div class="ml-3 text-sm">
+                      <label
+                        for="remember"
+                        class="text-gray-500 dark:text-gray-300"
+                        >Remember me</label
+                      >
+                    </div>
                   </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input  v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                  </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" >
-                          </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                          </div>
-                      </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                  </div>
-                  <moviecard />
-                  <button  @click="goToLogin" type="button" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have a favorit movie yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                  </p>
+                  <a
+                    href="#"
+                    class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Forgot password?</a
+                  >
+                </div>
+                <moviecard />
+                <button
+                  @click="goToLogin"
+                  type="button"
+                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
+                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don’t have a favorit movie yet?
+                  <a
+                    href="#"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >Sign up</a
+                  >
+                </p>
               </form>
+            </div>
           </div>
-      </div>
-  </div>
-</section>
         </div>
-        <NuxtLink class="text-green-500 font-bold " to="/practice1">login back</NuxtLink>
-
-        
+      </section>
     </div>
 
+  </div>
 </template>
